5f9f2b1e82c2fe6370ec390dd3ebd8083dba6173
eloviskis <eloi.santaroza@gmail.com>
2024-07-11 12:21:13 -0300

    modified:   footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-800 p-4 text-white text-center">
+    <div class="container mx-auto">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Todos os direitos reservados.</p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
